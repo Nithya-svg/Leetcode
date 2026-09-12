bbda9c0b48a890cb4761592714e0cc755039def3
Nithya-svg <nithyavarshini0504@gmail.com>
2026-09-12 22:54:40 +0530

    Time: 3 ms (69.5%), Space: 47.5 MB (62.97%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int i = 0;
+            while(i < nums.length){
+            int correctindex = nums[i] - 1;
+            if(nums[i] != nums[correctindex]){
+                int temp = nums[i];
+                nums[i] = nums[correctindex];
+                nums[correctindex] = temp;
+            }
+            else{
+                i++;
+                }
+
+            }
+            for(i=0; i < nums.length;i++){
+           
+            if(nums[i] != i+1){
+                return new int[]{nums[i],i+1};
+                
+            }
+        
+        }
+        return new int[]{-1,-1};
+    }
+}
